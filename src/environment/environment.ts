ee5d16542e47d58ddb828dc55da30d46942861ae
DavidOrozco74 <david.orozco@smartsoft.com.co>
2025-02-03 05:20:31 -0500

    url

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://todo-backend-qj9z.onrender.com/api/todos, // Cambia esto por la URL de tu backend en desarrollo
+    apiUrl: 'https://todo-backend-qj9z.onrender.com/api/todos', 
   };
